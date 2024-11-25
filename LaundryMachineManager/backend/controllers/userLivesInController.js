b90c685607c0fc65fac217fa0a475833e6796376
angelafeliciaa <angelafc@students.cs.ubc.ca>
2024-11-24 19:23:20 -0800

    return userdata

--- LaundryMachineManager/backend/controllers/userLivesInController.js
+++ LaundryMachineManager/backend/controllers/userLivesInController.js
@@ -6,21 +6,24 @@ const loginDefaultUser = async (req, res) => {
     return res.status(400).json({ error: "Email and password are required." });
   }
   try {
-    const { data, error } = await supabaseServiceRole
-      .from("userlivesin")
-      .select("upassword")
-      .eq("uemail", uemail)
-      .single();
+    const { data: user, error } = await supabaseServiceRole
+    .from('userlivesin')
+    .select('*')
+    .eq('uemail', uemail)
+    .eq('upassword', upassword)
+    .single();
 
-    if (error || !data) {
+    if (error || !user) {
       return res.status(401).json({ error: "Invalid email or password." });
     }
 
-    if (data.upassword !== upassword) {
-      return res.status(401).json({ error: "Invalid email or password." });
-    }
-
-    return res.status(200).json({ message: "Login successful!" });
+    return res.status(200).json({
+      uid: user.uid,
+      bid: user.bid,         
+      name: user.uname,      
+      email: user.uemail,
+      password: user.upassword
+    });
   } catch (err) {
     console.error("Error during login:", err.message);
     return res.status(500).json({ error: "Internal server error" });
