e648a3f3b73b4eb02eca8f7af7b6e9f5c561cb4f
angelafeliciaa <angelafc@students.cs.ubc.ca>
2024-11-24 19:24:36 -0800

    fetch balance works

--- LaundryMachineManager/frontend/src/components/home-page/funds-widget/FundsWidget.jsx
+++ LaundryMachineManager/frontend/src/components/home-page/funds-widget/FundsWidget.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ButtonLarge from "../../global/ButtonLarge";
 import AddFundsModal from "./AddFundsModal";
 
-export default function FundsWidget({ balance }) {
-  const [bal, setBal] = useState(balance);
+export default function FundsWidget({ uid }) {
+  const [balance, setBalance] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        const response = await fetch(`http://localhost:5001/washingCard/${uid}`);
+        const data = await response.json();
+        setBalance(data.balance);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchBalance();
+  }, [uid]);
 
   const toggleOpenClose = () => setIsModalOpen((prev) => !prev);
 
@@ -12,11 +25,11 @@ export default function FundsWidget({ balance }) {
     <section className="flex flex-col items-start justify-between w-[250px] bg-widget p-5 rounded-3xl">
       <div>
         <h1>Funds</h1>
-        <h1>${bal}</h1>
+        <h1>${balance.toFixed(2)}</h1>
       </div>
       <ButtonLarge name="Add Funds" onClick={toggleOpenClose} />
       {isModalOpen && (
-        <AddFundsModal onClose={toggleOpenClose} setBal={setBal} />
+        <AddFundsModal onClose={toggleOpenClose} setBal={setBalance} uid={uid} />
       )}
     </section>
   );
