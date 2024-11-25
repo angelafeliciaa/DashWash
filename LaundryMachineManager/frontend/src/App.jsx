cdbd035b5384184d44426cfe9416937007893250
angelafeliciaa <angelafc@students.cs.ubc.ca>
2024-11-24 19:23:41 -0800

    add new routes

--- LaundryMachineManager/frontend/src/App.jsx
+++ LaundryMachineManager/frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
-
 import "./App.css";
 import LoginCard from "./components/default-auth-page/LoginCard";
+import RegisterCard from "./components/default-auth-page/RegisterCard";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginCard />} />
+        <Route path="/register" element={<RegisterCard />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
     </Router>
