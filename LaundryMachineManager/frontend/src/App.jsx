fe34044aaf44f7c5aaa2c340d8dfafeaaea855fc
angelafeliciaa <angelafc@students.cs.ubc.ca>
2024-11-24 19:23:41 -0800

    add new routes

--- LaundryMachineManager/frontend/src/App.jsx
+++ LaundryMachineManager/frontend/src/App.jsx
@@ -4,12 +4,15 @@ import AuthPage from "./pages/AuthPage";
 import RmHomPage from "./pages/RmHomePage";
 import "./App.css";
 import LoginCard from "./components/default-auth-page/LoginCard";
+import RegisterCard from "./components/default-auth-page/RegisterCard";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginCard />} />
+        <Route path="/register" element={<RegisterCard />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/rm" element={<RmHomPage />} />
       </Routes>
