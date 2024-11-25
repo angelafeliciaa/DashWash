8818dbe4e4a05dc2764e03d51b27e3a92145e835
angelafeliciaa <angelafc@students.cs.ubc.ca>
2024-11-24 19:25:21 -0800

    try to fetch transaction history (not working yet)

--- LaundryMachineManager/frontend/src/components/home-page/card-history-widget/CardHistoryWidget.jsx
+++ LaundryMachineManager/frontend/src/components/home-page/card-history-widget/CardHistoryWidget.jsx
@@ -1,6 +1,29 @@
 import TransactionRow from "./TransactionRow";
+import { useState, useEffect } from "react";
 
-export default function CardHistoryWidget({ transactions }) {
+export default function CardHistoryWidget({ cid }) {
+    const [transactions, setTransactions] = useState([]);
+  
+    useEffect(() => {
+      const fetchTransactions = async () => {
+        try {
+          const response = await fetch(`http://localhost:5001/transactionHistory/${cid}`);
+          const data = await response.json();
+          if (response.ok) {
+            setTransactions(data.transactions);
+          } else {
+            console.error(data.error);
+          }
+        } catch (err) {
+          console.error("Failed to fetch transactions:", err);
+        }
+      };
+  
+      if (cid) {
+        fetchTransactions();
+      }
+    }, [cid]);
+
   return (
     <section className="flex flex-col h-full gap-2 w-full bg-widget p-5 rounded-3xl overflow-y-auto">
       <h1>Card History</h1>
