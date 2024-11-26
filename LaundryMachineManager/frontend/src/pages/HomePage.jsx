5b7308050becb88c6706cf44478ec270c523c2df
angelafeliciaa <angelafc@students.cs.ubc.ca>
2024-11-25 20:42:30 -0800

    transaction history

--- LaundryMachineManager/frontend/src/pages/HomePage.jsx
+++ LaundryMachineManager/frontend/src/pages/HomePage.jsx
@@ -85,6 +85,7 @@ const HomePage = () => {
     try {
       const response = await fetch(`http://localhost:5001/washingCard/${uid}/cardID`);
       const data = await response.json();
+      console.log("API Response Data:", data);
       if (response.ok) {
         setCid(data.cid);
       } else {
@@ -110,7 +111,7 @@ const HomePage = () => {
         <div className="flex-1 flex flex-col">
         <section className="ml-2 w-[500px] mt-2">
             <div className="flex items-center p-4 rounded-lg">
-              <h1>Card - {cid}</h1>
+              <h1>Welcome Back!</h1>
             </div>
           </section>
         {/* <CardDropdownWidget uid={userData.uid} /> */}
@@ -122,7 +123,15 @@ const HomePage = () => {
             <CurrMachineWidget />
           </div>
           <div className="flex-1 overflow-y-auto mt-2">
-            <CardHistoryWidget cid={cid}/>
+          <CardHistoryWidget
+              transactions={[
+                { tid: 1, day: "2024-11-16", time: "14:30:00", price: 2 },
+                { tid: 2, day: "2024-11-15", time: "10:00:00", price: 3 },
+                { tid: 3, day: "2024-11-14", time: "08:45:00", price: 5 },
+                { tid: 4, day: "2024-11-13", time: "20:15:00", price: 1 },
+                { tid: 5, day: "2024-11-12", time: "16:30:00", price: 4 },
+              ]}
+            />
           </div>
         </div>
         {/* Right Column */}
