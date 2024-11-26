900c55ccb29caacb9e7d8137ff29101639a04079
angelafeliciaa <angelafc@students.cs.ubc.ca>
2024-11-26 10:27:58 -0800

    from leo's branch

--- LaundryMachineManager/backend/routes/userLivesInRoute.js
+++ LaundryMachineManager/backend/routes/userLivesInRoute.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getUserLivesIn,
   loginDefaultUser,
+  rmDashBoardUsers,
 } = require("./../controllers/userLivesInController.js");
 
 const router = express.Router();
 
 router.get("/", getUserLivesIn);
 router.post("/loginDefault", loginDefaultUser);
+router.post("/rmDashBoard", rmDashBoardUsers);
 
 module.exports = router;
