0e3c65e435d4cd977f7b85b0ed46d9ca82f0ab64
Leo <leoshang12@gmail.com>
2024-11-28 00:29:03 -0800

    Bing chilling

--- LaundryMachineManager/frontend/src/components/home-page/CurrMachineWidget.jsx
+++ LaundryMachineManager/frontend/src/components/home-page/CurrMachineWidget.jsx
@@ -6,7 +6,7 @@ export default function CurrMachineWidget({ machines }) {
     <section className="flex flex-col justify-start w-full bg-widget rounded-3xl p-5">
       <h1 className="mb-2">Current Machine Usage</h1>
       <div className="flex gap-4 overflow-x-auto">
-        <DryerCard id={1} washingStatus="open" timeLeft={12} />
+        <DryerCard id={1} washingStatus="Available" timeLeft={12} />
         <WasherCard id={2} washingStatus="in use" timeLeft={32} />
       </div>
     </section>
