d4bbdc54e8af087eafe7acdc5a219fc5dd7ea57f
Leo <leoshang12@gmail.com>
2024-11-28 13:39:08 -0800

    Finished Up

--- LaundryMachineManager/backend/controllers/buildingController.js
+++ LaundryMachineManager/backend/controllers/buildingController.js
@@ -3,16 +3,44 @@ const { supabaseServiceRole } = require("../supabaseClient");
 const getBuildings = async (req, res) => {
   try {
     // fetch building data from CampusResidence
-    const { data, error } = await supabaseServiceRole.from("campusresidence").select("bid, bname");
+    const { data, error } = await supabaseServiceRole
+      .from("campusresidence")
+      .select("bid, bname");
 
     if (error) {
-      return res.status(400).json({ error: "Failed to fetch building data.", details: error.message });
+      return res.status(400).json({
+        error: "Failed to fetch building data.",
+        details: error.message,
+      });
     }
 
     res.status(200).json(data);
   } catch (err) {
-    res.status(500).json({ error: "Internal server error.", details: err.message });
+    res
+      .status(500)
+      .json({ error: "Internal server error.", details: err.message });
   }
 };
 
-module.exports = { getBuildings };
+const getBuildingsWithAvailMachines = async (req, res) => {
+  try {
+    const { data, error } = await supabaseServiceRole.rpc(
+      "get_buildings_with_available_machines"
+    );
+
+    if (error) {
+      return res.status(400).json({
+        error: "Failed to fetch buildings with available machines.",
+        details: error.message,
+      });
+    }
+
+    res.status(200).json(data);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Internal server error.", details: err.message });
+  }
+};
+
+module.exports = { getBuildings, getBuildingsWithAvailMachines };
